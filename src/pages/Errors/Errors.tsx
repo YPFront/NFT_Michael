f9718871c3035c1539f6a8ab2adb5e53256f19d0
Slava <pecheniuky@gmail.com>
2021-11-09 01:51:35 +0300

    Terms and 404

---
+++ src/pages/Errors/Errors.tsx
@@ -0,0 +1,50 @@
+import { makeStyles, ThemeProvider } from '@material-ui/core';
+import Footer from 'src/pages/Layout/Footer';
+import Header from 'src/pages/Layout/Header';
+import { theme as PortionTheme } from 'src/theme/theme';
+import ErrorsMain from '../Layout/ErrorsMain/ErrorsMain';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+    background: theme.palette.background.default,
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 20,
+    lineHeight: '24px',
+    [theme.breakpoints.down('lg')]: {
+      fontSize: 18,
+      lineHeight: '21.5px',
+      // '& p': {
+      //   marginBlockStart: 8,
+      //   marginBlockEnd: 8
+      // }
+    }
+  },
+  container: {
+    display: 'flex',
+    flex: 1,
+  },
+  main: {
+    flex: 1,
+  },
+}));
+
+type Props = {};
+
+export default function Transaction({}: Props) {
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={PortionTheme}>
+      <div className={classes.root}>
+        <Header login={true} color="primary"/>
+        <main className={classes.main}>
+          <ErrorsMain></ErrorsMain>
+        </main>
+      </div>
+    </ThemeProvider>
+  );
+}
